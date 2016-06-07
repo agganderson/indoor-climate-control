0cde79e079e59f5ba93ce3073d97e64551066617
Alexandra Anderson <aliganderson@gmail.com>
2016-06-07 11:33:09 -0700

    rearranging/deleting files and fixing styling

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 // require('./server/config/mongoose.js');
 // require('./server/config/routes.js')(app);
 
-app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(8000, function(){
 	console.log('Listening on port 8000');
